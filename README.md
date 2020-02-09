873c797c11788dab8ffdda33ffc4b49e72fb83d0
Andrey <i@dru.su>
2020-02-09 19:04:23 +0700

    Simple node.js REST server with ORM (on postgres)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple implementation Micro.js REST API
+
+* [Micro.js](https://github.com/zeit/micro)
+* [microrouter](https://github.com/pedronauck/micro-router)
+* [Sequelize](https://sequelize.org/v5/)
+* [pg](https://node-postgres.com/)
---
+++ src/db.js
@@ -0,0 +1,25 @@
+const { Sequelize, Model, DataTypes } = require('sequelize')
+
+
+const sequelize = new Sequelize('micro_intro', 'postgres', '123', {
+    dialect: 'postgres'
+})
+
+class Record extends Model {}
+
+Record.init(
+    {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+        text: DataTypes.STRING
+    },
+    {
+        sequelize,
+        modelName: 'record'
+    }
+)
+
+sequelize.sync()
+
+module.exports = {
+    Record,
+}
